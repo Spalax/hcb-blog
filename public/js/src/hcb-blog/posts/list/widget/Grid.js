5ba6a9f9d630afd7dfc0cb7065a17bc7a7d3c883
Milotskiy Alexey <spalaxinco@gmail.com>
2014-01-17 16:30:26 +0200

    Fixed bug with NO Data message when data exists

--- public/js/src/hcb-blog/posts/list/widget/Grid.js
+++ public/js/src/hcb-blog/posts/list/widget/Grid.js
@@ -7,6 +7,7 @@ define([
     "dgrid/extensions/ColumnResizer",
     "dgrid/extensions/DijitRegistry",
     "hc-backend/dgrid/_Selection",
+    "hc-backend/dgrid/_Refresher",
     "hc-backend/dgrid/columns/timestamp",
     "hc-backend/dgrid/columns/editor",
     "dgrid/Keyboard",
@@ -14,10 +15,11 @@ define([
     "dojo/i18n!../../../nls/List"
 ], function(declare, lang, PostsStore,
             OnDemandGrid, ColumnHider, ColumnResizer, DijitRegistry,
-            _Selection, timestamp, editor, Keyboard, selector, translation) {
+            _Selection, _Refresher, timestamp, editor, Keyboard,
+            selector, translation) {
     
     return declare([ OnDemandGrid, ColumnHider, ColumnResizer,
-                     Keyboard, _Selection, DijitRegistry ], {
+                     Keyboard, _Selection, _Refresher, DijitRegistry ], {
         //  summary:
         //      Grid widget for displaying all available clients
         //      as list
